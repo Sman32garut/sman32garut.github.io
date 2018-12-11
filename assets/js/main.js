c7962a2b57f36713d4bd3fe0bdc17dbb8d911214
restu suhendar <restu.suhendar@gmail.com>
2018-12-11 12:44:28 +0700

    add visi misi

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,4 +14,39 @@ var NavbarCollapse = function () {
             e.removeClass("collapsing-out"), a.removeClass("navbar-collapsed-out"), t.removeClass("navbar-collapsed-out")
         }
     }))
+}(),NavbarSticky = function () {
+    var a = $(".navbar-sticky");
+
+    function e(a) {
+        var e = $(window).scrollTop();
+        t + 200 < e ? a.addClass("sticky") : a.removeClass("sticky")
+
+        t + 200 < e ? $(".navbar-brand img").height("40px") : $(".navbar-brand img").height("60px")
+    }
+
+    if (a.length) {
+        var t = a.offset().top;
+        e(a), $(window).on({
+            scroll: function () {
+                e(a)
+            }
+        })
+    }
+}(), CardActions = function () {
+    var a = $(".card"), n = ".card-product-actions";
+    a.length && $(n).length && (a.on({
+        mouseenter: function () {
+            var a, e, t;
+            a = $(this), e = a.find(n), t = e.data("animation-in"), e.length && (e.addClass("in animated " + t), e.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function () {
+                e.removeClass("animated " + t)
+            }))
+        }
+    }), a.on({
+        mouseleave: function () {
+            var a, e, t;
+            a = $(this), e = a.find(n), t = e.data("animation-out"), e.length && (e.addClass("animated " + t), e.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function () {
+                e.removeClass("in animated " + t)
+            }))
+        }
+    }))
 }();
