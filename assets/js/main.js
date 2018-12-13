e88822a82e6dced1bcf0f6a7fd25e3a7fa763747
restu suhendar <restu.suhendar@gmail.com>
2018-12-13 12:21:57 +0700

    add gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,5 +56,85 @@ var NavbarCollapse = function () {
             $('body').addClass('loaded');
         }, 6000);
 
+
+    });
+}(), gallery = function () {
+    $(document).ready(function () {
+        $.getJSON( "/config/ig/ig.json", function( data ) {
+            // grab out load more button
+            var loadButton = document.getElementById('gallery-load-more');
+            var feed = new Instafeed({
+                get: 'user',
+                userId: data.userId,
+                clientId: data.clientId,
+                accessToken: data.accessToken,
+                resolution: "standard_resolution",
+                sortBy: "most-recent",
+                limit: 4,
+                filter: function (image) {
+                    console.log(image);
+                    return true;
+                },
+                template: '<div class="col-lg-3 mb-5 mb-lg-0"> <div data-animate-hover="1" data-toggle="hidden"> ' +
+                '<div class="card shadow animate-this card-visi"> <a href="{{image}}" data-caption="{{caption}}">' +
+                '<img src="{{image}}" alt="{{caption}}" /></a> </div> </div> </div>',
+                after: function() {
+                    // disable button if no more results to load
+                    if (!this.hasNext()) {
+                        loadButton.setAttribute('disabled', 'disabled');
+                    }
+                }
+            });
+
+            // bind the load more button
+            loadButton.addEventListener('click', function() {
+                feed.next();
+            });
+
+            feed.run();
+
+            var loadButtonPengajar = document.getElementById('pengajar-load-more');
+            var feedPengajar = new Instafeed({
+                get: 'user',
+                userId: data.userId,
+                clientId: data.clientId,
+                accessToken: data.accessToken,
+                resolution: "standard_resolution",
+                sortBy: "least-recent",
+                target: "instafeed-pengajar",
+                filter: function (image) {
+                    return image.tags.indexOf('pengajar') >= 0;
+                },
+                template: '<div class="col-lg-3 mb-5 mb-lg-0"> <div data-animate-hover="1" data-toggle="hidden"> ' +
+                '<div class="card shadow animate-this card-visi"> <a href="{{image}}" data-caption="{{caption}}">' +
+                '<img src="{{image}}" alt="{{caption}}" /></a> </div> </div> </div>',
+                after: function() {
+                    // disable button if no more results to load
+                    if (!this.hasNext()) {
+                        loadButton.setAttribute('disabled', 'disabled');
+                    }
+                }
+            });
+
+            // bind the load more button
+            loadButtonPengajar.addEventListener('click', function() {
+                feedPengajar.next();
+            });
+
+            feedPengajar.run();
+        });
+
+        function cek_gallery(last_gallery) {
+
+            if ($(".gallery a").length > last_gallery) {
+                baguetteBox.run('.gallery');
+                last_gallery = $(".gallery a").length;
+            }
+
+            setTimeout(function(){
+                cek_gallery(last_gallery);
+            }, 1000);
+        }
+        cek_gallery(0);
     });
 }();
